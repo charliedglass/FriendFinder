7b9ceecef5126a315ae58f182365353f9e011467
Charlie Glass <charliedglass@gmail.com>
2019-03-17 22:50:35 -0700

    adding all files for the FriendFinder app

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//sample user (me) to start
+var userArray = [
+    {
+        name: "Charlie Glass",
+        photo: "https://charliedglass.github.io/Bootstrap-Portfolio/assets/images/profile_pic.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    }
+];
+
+module.exports = userArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,33 @@
+var userData = require("../data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res){
+        res.json(userData);
+    });
+
+    app.post("/api/friends", function(req, res){
+        var surveyAnswers = req.body;
+        var friendMatch = userData[0];
+        var minDifference = 0;
+
+        for (var i = 0; i < userData.length; i++){
+            var totalDifference = 0;
+            for (var j = 0; j < userData[i].scores.length; j++){
+                console.log(surveyAnswers.scores[j]);
+                console.log(parseInt(surveyAnswers.scores[j]));
+                totalDifference += Math.abs(parseInt(surveyAnswers.scores[j])-parseInt(userData[i].scores[j]))
+                console.log(totalDifference);
+            }
+            if (totalDifference < minDifference || i == 0){
+                friendMatch = userData[i];
+                minDifference = totalDifference;
+            }
+        }
+        console.log(friendMatch);
+        console.log(minDifference);
+        userData.push(surveyAnswers);
+        res.json(friendMatch);
+
+    })
+}
+
