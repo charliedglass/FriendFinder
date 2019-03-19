bbfae3964887eff179d3f0d64e4acec970471140
Charlie Glass <charliedglass@gmail.com>
2019-03-18 18:29:20 -0700

    adding readme to friendfinder app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FriendFinder
+In the FriendFinder App, we use `express` to handle routes and match you with your closest friends!
+
+You have the option to go to the survey page from the home page.  After you fill out all the required fields in the survey, a modal pops up suggesting the best friend match for you in the database.
+
+You can see the list of all the possible friends by clicking on the link `API Friends List` on the home or survey page.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,18 +14,15 @@ module.exports = function(app) {
         for (var i = 0; i < userData.length; i++){
             var totalDifference = 0;
             for (var j = 0; j < userData[i].scores.length; j++){
-                console.log(surveyAnswers.scores[j]);
-                console.log(parseInt(surveyAnswers.scores[j]));
+                //Since the inputs are strings, we must parse the int to perform the mathematical operation
                 totalDifference += Math.abs(parseInt(surveyAnswers.scores[j])-parseInt(userData[i].scores[j]))
-                console.log(totalDifference);
             }
             if (totalDifference < minDifference || i == 0){
                 friendMatch = userData[i];
                 minDifference = totalDifference;
             }
         }
-        console.log(friendMatch);
-        console.log(minDifference);
+        
         userData.push(surveyAnswers);
         res.json(friendMatch);
 
